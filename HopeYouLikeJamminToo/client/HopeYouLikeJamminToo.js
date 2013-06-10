8faaab51430bb9d567f017c08790d7ef32cc4647
Dylan O'Carroll <dylanocarroll@Dylans-Laptop.local>
2013-06-09 18:57:15 -0700

    working on showing stuff from DB.

--- HopeYouLikeJamminToo/client/HopeYouLikeJamminToo.js
+++ HopeYouLikeJamminToo/client/HopeYouLikeJamminToo.js
@@ -1,5 +1,6 @@
 var Tracks = new Meteor.Collection("tracks");
 
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to HopeYouLikeJamminToo.";
@@ -14,44 +15,58 @@ if (Meteor.isClient) {
   });
 
   Template.sound.trackaroonies = function(){
-  	return Tracks.find();
+    return Tracks.find();
   }
 
   var streaming;
+  // var elRecordo;
+
+  // var insertIntoDatabase = function() {
+  //   recorder.exportWAV(function(blob) {
+  //     Tracks.insert({
+  //       blob: blob
+  //     });
+  //   });
+  // };
+
+  var createDownloadLink = function() {
+    recorder && recorder.exportWAV(function(blob) {
+      var url = URL.createObjectURL(blob);
+      var li = document.createElement('li');
+      var au = document.createElement('audio');
+      var hf = document.createElement('a');
+
+      au.controls = true;
+      au.src = url;
+      hf.href = url;
+      hf.download = new Date().toISOString() + '.wav';
+      hf.innerHTML = hf.download;
+      li.appendChild(au);
+      li.appendChild(hf);
+      recordingslist.appendChild(li);
+    });
+  }
+
 
   Template.sound.events({
     'click button': function() {
-
       if(streaming){
-      	streaming.stop();
-      	// streaming = undefined;
-      	console.log(streaming);
-      	streaming = undefined;
+      	recorder.stop();
+      	console.log(recorder);
+        // streaming.stop();
+
+        createDownloadLink();
+        // insertIntoDatabase();
+
+        recorder.clear();
+
+        streaming = false;
       } else {
 
-      	window.URL = window.URL || window.webkitURL;
-	      navigator.getMedia = navigator.getUserMedia ||
-	                            navigator.webkitGetUserMedia ||
-	                            navigator.mozGetUserMedia ||
-	                            navigator.msGetUserMedia;
-	      navigator.getMedia(
-	        { audio: true },
-
-		    function(localMediaStream){
-		    	streaming = localMediaStream;
-		      var audio = document.querySelector('audio');
-		      localMediaStream.onended = function(){
-		      	console.log("onended called");
-		      	Tracks.insert(window.URL.createObjectURL(localMediaStream));
-		      	audio.src = window.URL.createObjectURL(localMediaStream);
-		      }
-		      audio.onloadedmetadata = function(e){
-		        //console.log(e);
-		      };
-		    },
-		    function(err){ console.log("error: ", err); }
-        );
-	    }
+      	recorder.record();
+      	console.log(recorder);
+      	streaming = true;
+      }
     }
   });
 };
